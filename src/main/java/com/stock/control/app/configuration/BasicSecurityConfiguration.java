9780b89b084b4eb261b65da6d065e59abb34136c
Jonatan <costalesjonatan@gmail.com>
2024-08-08 11:17:28 -0300

    Basic security configuration and userEntity updated.

--- src/main/java/com/stock/control/app/configuration/BasicSecurityConfiguration.java
+++ src/main/java/com/stock/control/app/configuration/BasicSecurityConfiguration.java
@@ -4,19 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class BasicSecurityConfiguration {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/auth/**").permitAll()
-                .anyRequest().authenticated());
-        http.httpBasic(Customizer.withDefaults());
+        http
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                );
         return http.build();
     }
 
